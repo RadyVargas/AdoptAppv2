628d28e0d62b1961963f01c71533fbe1625f7f70
Rady Vargas <rady.vargass@gmail.com>
2025-11-18 19:39:12 -0300

    Agrego el middleware

--- adopcion/settings.py
+++ adopcion/settings.py
@@ -35,6 +35,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -115,6 +116,8 @@ STATICFILES_DIRS = [
 
 # Carpeta donde se recopilarán todos los archivos estáticos para producción
 STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
