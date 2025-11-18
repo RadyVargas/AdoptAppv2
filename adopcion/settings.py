a04c1b638bb4b975f283b19e50f44dec202917f7
Rady Vargas <rady.vargass@gmail.com>
2025-11-18 19:29:35 -0300

    Nuevo gitignore limpio

--- adopcion/settings.py
+++ adopcion/settings.py
@@ -15,7 +15,10 @@ SECRET_KEY = 'django-insecure-x4y_duv1m!5i*1hnx*z)vp_5h#o!fib5+)j2og%0)&xel5=vvh
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [] #solo para demostracion 'adoptapp-702k.onrender.com'
+ALLOWED_HOSTS = [    
+    "localhost",
+    "127.0.0.1",
+    "adoptappv2.onrender.com",] #solo para demostracion 'adoptapp-702k.onrender.com'
 
 
 # Application definition
